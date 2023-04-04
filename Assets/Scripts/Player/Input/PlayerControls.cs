1de77305bd9eb2e9666d2a1251e7db0327de08cc
Scott Murphy <scottymurph7@gmail.com>
2023-04-04 02:57:52 -0400

    beta 0.0.2
    Moving!

--- Assets/Scripts/Player/Input/PlayerControls.cs
+++ Assets/Scripts/Player/Input/PlayerControls.cs
@@ -1,18 +1,111 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerControls : MonoBehaviour
+public class PlayerControls : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerInput playerInput;
+    Rigidbody2D rb;
+
+    public bool isBuilding;
+
+    public float walkSpeed;
+    public float runSpeed;
+    float moveSpeed;
+
+    Vector2 scrollValue;
+
+    void Awake()
     {
-        
+        moveSpeed = walkSpeed;
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        InitPlayerActions();
+    }
+
+    void InitPlayerActions()
+    {
+        playerInput.actions["HitPlace"].started += ctx => Hit();
+        playerInput.actions["Rotate"].started += ctx => Rotate();
+        playerInput.actions["Craft"].started += ctx => OpenCraft();
+        playerInput.actions["Inventory"].started += ctx => OpenInventory();
+        playerInput.actions["Map"].started += ctx => OpenMap();
+        playerInput.actions["Interact"].started += ctx => Interact();
+        playerInput.actions["Reload"].started += ctx => Reload();
+        playerInput.actions["ChangeAnchor"].started += ChangeAnchor;
+    }
+
+    private void Update()
+    {
+        if (IsOwner) Move();
+    }
+
+    void Move()
+    {
+        Vector2 moveDir = playerInput.actions["Move"].ReadValue<Vector2>();
+
+        rb.velocity = moveDir * moveSpeed;
+    }
+
+    void Hit()
+    {
+        if (isBuilding)
+        {
+            Build();
+            return;
+        }
+
+
+    }
+
+    void Build()
+    {
+
+    }
+
+    void Rotate()
+    {
+        if (!isBuilding) return;
+
+
+    }
+
+    void OpenCraft()
+    {
+
+    }
+
+    void OpenInventory()
+    {
+
+    }
+
+    void OpenMap()
+    {
+
+    }
+
+    void Interact()
+    {
+
+    }
+
+    void Reload()
+    {
+
+    }
+
+    private void ChangeAnchor(InputAction.CallbackContext ctx)
+    {
+        scrollValue = ctx.ReadValue<Vector2>();
+
+
     }
 }
